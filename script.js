a552779ee7b72ebba55db2b16eb821e933c629a3
Nikhil Tiwary <ntiwary61@gmail.com>
2023-03-17 14:46:07 +0530

    feat: dynamic change in grid size according to user's input

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
+let size = 16;
+let grid_dimension = (500 - (size - 1)) / size;
+createGrid(size);
+
 function askSize() {
-  let size = prompt("Enter grid size");
+  size = prompt("Enter grid size");
+  if (size != null) {
+    grid_dimension = (500 - (size - 1)) / size;
+    createGrid(size);
+  }
+}
+function clearGrid() {
+  const myGrid = document.getElementById("sketch-grid");
+  while (myGrid.firstChild) {
+    myGrid.removeChild(myGrid.lastChild);
+  }
+}
+function createGrid(size) {
+  clearGrid();
+  let space = document.getElementById("sketch-grid");
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    for (let j = 0; j < size; j++) {
+      let col = document.createElement("div");
+      col.className = "gridbox";
+      col.style.width = `${grid_dimension}px`;
+      col.style.height = `${grid_dimension}px`;
+      row.appendChild(col);
+      row.style["display"] = "flex";
+    }
+    space.appendChild(row);
+  }
 }
 
 let grid_btn = document.getElementById("grid-size");
